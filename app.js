e32f6b469572d0d004a677a9bcaecd4b2af8d440
rahul-rathore786 <rahulrathorese786@gmail.com>
2024-03-29 22:46:41 +0530

    define port

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const app = express();
 // const port = 3000;
 
 // define port
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
+
 // create mongoose atlas connection
 const mongoose = require("mongoose");
 require("dotenv").config(); // Load environment variables from .env file
